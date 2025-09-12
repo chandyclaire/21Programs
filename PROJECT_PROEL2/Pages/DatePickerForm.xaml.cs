6213010a6064896a1029b92b0d8709fdf37c641b
chandyclaire <chandyclaire282005@gmail.com>
2025-09-12 16:55:49 +0800

    Add: DatePickerForm

---
+++ PROJECT_PROEL2/Pages/DatePickerForm.xaml.cs
@@ -0,0 +1,23 @@
+namespace PROJECT_PROEL2;
+
+public partial class DatePickerForm : ContentPage
+{
+	public DatePickerForm()
+	{
+		InitializeComponent();
+
+        DatePicker.Date = DateTime.Today;
+        UpdateLabel(DatePicker.Date);
+    }
+
+    private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        UpdateLabel(e.NewDate);
+    }
+
+    private void UpdateLabel(DateTime date)
+    {
+        
+        SelectDateLabel.Text = $"Selected date: {date:D}";
+    }
+}
