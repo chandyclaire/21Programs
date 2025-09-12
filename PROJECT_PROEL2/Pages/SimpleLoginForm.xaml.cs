8c5cc61fbd84c9b236b8605e5672c0568bf439b8
mnemosyne9643 <zoiegracedepaz04@gmail.com>
2025-09-12 14:52:09 +0800

    Add: Login

--- PROJECT_PROEL2/Pages/SimpleLoginForm.xaml.cs
+++ PROJECT_PROEL2/Pages/SimpleLoginForm.xaml.cs
@@ -6,4 +6,21 @@ public partial class SimpleLoginForm : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnLoginClicked(object sender, EventArgs e)
+    {
+        string username = UsernameEntry.Text;
+        string password = PasswordEntry.Text;
+
+        if (username == "admin" && password == "admin")
+        {
+            DisplayAlert("Login Success", $"Welcome,{username}!","OK");
+        }
+        else
+        {
+            ResultLabel.TextColor = Colors.Red;
+            ResultLabel.Text = "Invalid credentials.";
+        }
+
+    }
 }
