a8f3a81811afd1a151f6896d290e0711150e11ec
timocheu <ccs.timotheoche@gmail.com>
2025-09-15 16:59:07 +0800

    add something

--- PROJECT_PROEL2/Pages/ContactForm.xaml.cs
+++ PROJECT_PROEL2/Pages/ContactForm.xaml.cs
@@ -6,4 +6,23 @@ public partial class ContactForm : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Continue_Click(object sender, EventArgs e)
+    {
+		Contact currentContact = new Contact()
+		{
+			Email = entry_Email.Text,
+			Message = entry_Message.Text,
+			Name = entry_Name.Text,
+			Phone = entry_Phone.Text,
+		};
+			
+    }
+}
+public class Contact
+{
+    public string Email { get; set; }
+    public string Message { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
 }
